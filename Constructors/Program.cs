8bf63d6776863e22fe7f4359ce4301d5d9bd4c05
Salih Burgul <salihburgul98@gmail.com>
2021-11-28 18:24:15 +0300

    4.ders

---
+++ Constructors/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Consturactors
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            //Customer customer1 = new Customer { Id = 1, FirstName = "Engin", LastName = "Demiroğ", City = "Ankara" };
+            Customer customer2 = new Customer(2, "Derin", "Demiroğ", "Ankara");
+        }
+    }
+    
+    class Customer
+    {//ctor consturactor kısaltmasıdır.
+        //yukarıdaki işlem ile aşağıdaki işlemi çalıştırırız.
+        //default consturactır
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id= id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;    
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Generics
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {//generic yapılarda parantez içine hani tipi verirsek onu çalştırır
+            List<string> sehirler = new List<string>();
+            //count property dir
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+            Console.WriteLine(sehirler.Count);//count property dir
+            
+            MyList<string> sehirler2= new MyList<string>();
+            sehirler2.Add("Ankara");
+            sehirler2.Add("Ankara");
+            sehirler2.Add("Ankara");
+            sehirler2.Add("Ankara");
+            sehirler2.Add("Ankara");
+            sehirler2.Add("Ankara");
+            Console.WriteLine(sehirler2.Count);
+            
+
+            
+        }
+        
+        
+    }
+    class MyList<T> //generic class
+    {
+        T[] _array;
+        T[] _tempArray;
+        public MyList()
+        {
+            _array = new T[0];
+        }
+        public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length+1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+
+            }
+            //-1 kümenin son elemanı anlamı anlamına geliyor ve ona kadar ekliyor
+            _array[_array.Length-1] = item;
+        }
+        
+
+        public int Count
+        {
+            get { return _array.Length; }
+            
+        }
+
+    }
+}
