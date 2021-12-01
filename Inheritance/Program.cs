9151c3252a912790025ead6c2238e9e96c05ff00
Salih Burgul <salihburgul98@gmail.com>
2021-12-02 00:09:08 +0300

    2.12.2021

---
+++ Inheritance/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3];
+            {
+                new Customer { FirstName = "Engin" }; 
+                new Student { FirstName = "Derin" }; 
+                new Person { FirstName = "Salih" };
+            }
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+        }
+    }
+    class Person
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Customer : Person
+    {
+        public string City { get; set; } 
+    }
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Interfaces
+{//aynı operasyonu farklı yerlerde yapmak için interface kullanırız.
+    class Program
+    {//interface newlenemez
+        //interfaceler classların referansını tutabilirler
+        static void Main(string[] args)
+        {
+            IPersonManager customerManager = new CustomerManager();
+            customerManager.Add();
+
+            IPersonManager employeeManager = new EmployeeManager(); 
+            employeeManager.Add();
+            
+        }
+    }
+    interface IPersonManager
+    {//unimplemented Operation, tamamlanmış operasyon
+        void Add();
+        //müşteri ekleme kodları
+        void Update();
+        
+
+    }
+    //inherits - class --------------implements - interface
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Müsteri Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müsteri Güncellendi");
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {//personel eklme koları 
+        public void Add() 
+        
+        { 
+            Console.WriteLine("Personel Eklendi"); 
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personel Eklendi");
+
+        }
+        class InternManager : IPersonManager
+        {
+            public void Add()
+            {
+                Console.WriteLine("Stajyer Eklendi");
+            }
+
+            public void Update()
+            {
+                Console.WriteLine("Stajyer Güncellendi");
+            }
+        }
+
+        class ProjectManager
+        {
+            public void Add(IPersonManager personManager)
+            {
+                
+                personManager.Add();
+            }
+        }
+    }
+}
